a4f24faa49c908882c6c6eb039692a7c61c42479
Jared Bienz [MSFT] <jbienzms@hotmail.com>
2015-08-19 16:25:52 -0500

    Added ability for additional content to be added to packages. Added a dependency on IoT for the library.

--- Packaging/BuildAndPackage.cmd
+++ Packaging/BuildAndPackage.cmd
@@ -10,6 +10,8 @@ REM Clear Packages directory
 IF EXIST Packages (RMDIR Packages /s /q) 
 MKDIR Packages
 
+ECHO Copying Package Content
+XCOPY Content\*.* /s Builds
 
 ECHO Packaging
 nuget pack Microsoft.IoT.Devices.nuspec -OutputDirectory Packages
